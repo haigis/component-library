f33695784eb76b810e15b68943590fd4b906f05e
haigis <markhaig@outlook.com>
2024-06-26 21:52:18 +0100

    Version update in package.json.

--- dist/cjs/types/components/Button/Button.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './Button.css';
-interface ButtonProps {
-    ariaLabel: string;
-    text: string;
-    href: string;
-}
-declare const Button: (props: ButtonProps) => React.JSX.Element;
-export default Button;
